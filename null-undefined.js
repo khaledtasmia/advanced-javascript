3f53e326f0aea01f9eccb9df55f81bde6f9cf544
Tasmia Khaled <khaled.tasmia33@gmail.com>
2021-02-03 20:34:58 +0600

    null vs undefined cleared for interview

---
+++ null-undefined.js
@@ -0,0 +1,17 @@
+//undefined
+let pakhi;
+function add(num1, num2){
+    console.log(num1+num2);
+}
+
+function add(num1, num2) {
+    console.log(num1, num2);
+}
+
+const premik = {name:"smart dude", phone:456781};
+ console.log(premik.gf);
+
+let fun = undefined;
+ console.log(fun);
+
+ //null
